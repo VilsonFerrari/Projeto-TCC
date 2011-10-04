dfe291da96075218ff14f0f407caf69b9c3d4990
BubaAlun <aluno@ubuntu.(none)>
2011-10-03 22:01:30 -0300

    Reajustando Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoTCC::Application.config.secret_token = 'b79ce34758887782892847ba1c179050c3141299179b64e1cc25ed13fa1cf8054124d237700c5b0da0f893d3e8f92a0fb249c4267646b5032278a084cd73f001'
